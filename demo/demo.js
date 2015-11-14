8b0e1305103730d505d0b41c1d5351b17d8c511c
Pedro Ladaria <pedro.ladaria@gmail.com>
2015-11-14 23:19:27 +0100

    create demo

---
+++ demo/demo.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import shortnames from 'emoji-shortnames';
+import {emojify} from '../src/react-emojione';
+import 'babel-es6-polyfill/polyfill';
+
+const PlayGround = React.createClass({
+    styles: {
+        container: {display: 'flex'},
+        left: {minWidth: '400px', marginRight: '16px'},
+        textarea: {width: '100%', boxSizing: 'border-box', minHeight: '100px', marginTop: '10px'},
+        right: {minWidth: '400px'},
+        result: {whiteSpace: 'pre-wrap', marginTop: '10px'}
+    },
+    getInitialState() {
+        return {
+            input: 'Hello world! 😍😎😏:smile_cat::family:'
+        }
+    },
+    handleChange(event) {
+        this.setState({input: event.target.value})
+    },
+    render() {
+        return (
+            <div style={this.styles.container}>
+                <div style={this.styles.left}>
+                    <strong>Paste here text, emojis or :shortcodes:</strong>
+                    <textarea
+                        onChange={this.handleChange}
+                        style={this.styles.textarea}
+                        value={this.state.input}
+                    />
+                </div>
+                <div style={this.styles.right}>
+                    <strong>Result</strong>
+                    <div style={this.styles.result}>
+                        {emojify(this.state.input)}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
+
+const EmojiCategory = React.createClass({
+    render() {
+        return (
+            <div>
+                <h1>{this.props.title}</h1>
+                <div>
+                    {emojify(this.props.text)}
+                </div>
+            </div>
+        );
+    }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const categories = Object.keys(shortnames).map((category, i) =>
+        <EmojiCategory key={i} title={category} text={shortnames[category].join('')} />
+    );
+
+    ReactDOM.render(
+        <div>
+            <PlayGround />
+            {categories}
+        </div>,
+        document.getElementById('emojis')
+    );
+
+});
